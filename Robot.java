4b785854285fec6b44c2d96f5c2807a3a79d6a85
SimplyRed900 <a_skoko@live.concordia.ca>
2023-07-06 11:19:19 -0400

    Creation of classes and function skeleton

---
+++ Robot.java
@@ -0,0 +1,6 @@
+public class Robot {    
+    public void move() {
+        System.out.println("> Robot moves");
+    }
+    
+}
---
+++ Room.java
@@ -0,0 +1,14 @@
+/*The purpose of this file is to show the room class and its methods
+  This also includes the functions that will dynamically generate a room
+  size based on the user input. 
+*/
+import java.util.Random;
+import java.util.Scanner;
+public class Room { 
+    //this takes 2 user inputs then creates a 2D array starting from [0,0]-> [x,y]
+    public void generate_room(int x, int y){   
+        
+
+    }
+
+}
